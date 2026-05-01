3248081e99da1deb16d37872de062fd450bf9335
KubaTaba1uga <luna7337anul@proton.me>
2026-05-01 21:53:55 +0200

    fix bug

--- tasks.py
+++ tasks.py
@@ -38,11 +38,9 @@ def install(c):
 
 @task
 def build_bsp(c, config="stm32mp135d_odyssey_prod_defconfig"):
-    """
-    @todo get urls and versions from config file
-    """
-    repos =_find_repos_in_br_config(config)
     _pr_info(f"Building BSP...")
+
+    repos =_find_repos_in_br_config(config)
 
     if "debug" in config:
         to_download = repos.items()
@@ -51,13 +49,16 @@ def build_bsp(c, config="stm32mp135d_odyssey_prod_defconfig"):
             (repo, data) for (repo, data) in repos.items() if repo == "buildroot"
         ]
 
+    print(f"{to_download=}")
     c.run("mkdir -p third_party")
     with c.cd("third_party"):
         for repo, rdata in to_download:
             if os.path.exists(os.path.join(ROOT_PATH, "third_party", repo)):
                 continue
-            print(f"{repo=}", f"{rdata=}")
+
+            print(f"{repo=}")
             c.run(f"git clone {rdata['url']} {repo}")
+            print(f"{repo=}")            
             with c.cd(repo):
                 c.run(f"git checkout {rdata['version']}")
                 
@@ -271,10 +272,16 @@ def _find_repos_in_br_config(config: str):
     config_path = os.path.join(ROOT_PATH, "configs", config)
     config_dict = _parse_config(config_path)
 
-    results = {}
+    results = {
+        "buildroot": {
+            "version": "st/2024.02.9",
+            "url": "https://github.com/bootlin/buildroot.git",
+        },
+    }
     for repo_name, repo_dict in repo_version_map.items():
         url = config_dict.get(repo_dict["url"])
         version = config_dict.get(repo_dict["version"])
+
         if not (url and version):
             continue
 
@@ -284,7 +291,8 @@ def _find_repos_in_br_config(config: str):
 
 
 def _parse_config(config_path: str) -> dict:
-    result = {}
+    result = {
+    }
 
     with open(config_path, "r") as config_fp:
         config = config_fp.readlines()
